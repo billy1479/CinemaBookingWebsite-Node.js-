15f4cdec552338c4e1ca8db0b613c511b9413ac0
billy1479 <59696174+billy1479@users.noreply.github.com>
2023-01-10 23:40:02 +0000

    making frame

---
+++ JS/main.js
@@ -0,0 +1,4 @@
+// function for changing pages will go here
+// will need to manipulate CSS - thats it
+
+// functoions for dropdowns will go here in order to open them - check w3schools for dropdown code
