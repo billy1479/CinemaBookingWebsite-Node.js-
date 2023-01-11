6bef7e52823e7ee51894394b6e28b181073f3cd6
billy1479 <59696174+billy1479@users.noreply.github.com>
2023-01-11 23:10:30 +0000

    made basic frame

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,13 @@
 // function for changing pages will go here
 // will need to manipulate CSS - thats it
 
-// functoions for dropdowns will go here in order to open them - check w3schools for dropdown code
+// functions for dropdowns will go here in order to open them - check w3schools for dropdown code
+
+// This is for changing the tabs on the main page
+function changeTab(tabName) {
+    tabs = document.getElementsByClassName('page');
+    for (i=0;i<tabs.length;i++) {
+        tabs[i].style.display = 'none';   
+    }
+    document.getElementById(tabName).style.display = 'block';
+}
