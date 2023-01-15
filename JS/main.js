f268bc979433d25cbd9b9cc5f624f4f8ef6c5aaa
billy1479 <59696174+billy1479@users.noreply.github.com>
2023-01-15 22:42:22 +0000

    booking form

--- JS/main.js
+++ JS/main.js
@@ -39,7 +39,38 @@ function decreaseSlide() {
     changeSlide()
 }
 
-function pageLoad() {
-    changeTab('page1')
-    increaseSlide()
+// for the form functions
+var formSlide = -1;
+function changeFormSlide() {
+    slides = document.getElementsByClassName('formSlide');
+    for (i=0;i<slides.length;i++) {
+        slides[i].style.display = 'none';
+    }
+    // remove form change button when end of form is reached
+    if (formSlide == 3) {
+        document.getElementById('formChangeButtonRight').style.display = 'None';
+    } else if (formSlide == 0) {
+        document.getElementById('formChangeButtonLeft').style.display = 'None';
+    } else {
+        document.getElementById('formChangeButtonLeft').style.display = 'Block';
+        document.getElementById('formChangeButtonRight').style.display = 'Block';
+    }
+    slides[formSlide].style.display = 'Block';
 }
+
+function nextFormSlide() {
+    formSlide += 1;
+    changeFormSlide();
+}
+
+function previousFormSlide() {
+    formSlide -= 1;
+    changeFormSlide();
+}
+
+function pageLoad() {
+    changeTab('page1');
+    increaseSlide();
+    nextFormSlide();
+}
+
