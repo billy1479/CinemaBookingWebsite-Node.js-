28a09eca84c11d8d24605241e2009c2d29949873
billy1479 <59696174+billy1479@users.noreply.github.com>
2023-01-18 21:46:25 +0000

    h

--- JS/main2.js
+++ JS/main2.js
@@ -1,7 +1,8 @@
 // For Server-functions
-rootUrl = 'http://127.0.0.1/5500/';
+rootUrl = 'http://127.0.0.1:5500/';
 async function loadCurrentFilms() {
-    const currentFilmsObject = await fetch(rootUrl + '/currentFilms');
+    const currentFilmsObject = await fetch(rootUrl + 'currentFilms');
+    const currentFilmsResponse = await currentFilmsObject.text();
     console.log(currentFilmsObject);
 }
 loadCurrentFilms();
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app.js')
 const hostname = '127.0.0.1';
-const port = 5501;
+const port = 5500;
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
