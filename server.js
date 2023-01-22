8d944610598fe1f16bae89e8faca3bd15690bfd1
billy1479 <59696174+billy1479@users.noreply.github.com>
2023-01-22 19:02:20 +0000

    all testing done

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const app = require('./app.js')
+const app = require('./app.js');
 const hostname = '127.0.0.1';
 const port = 5500;
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
-
