afd115808cc4c8dbacb912fed02cae7dcc79037e
billy1479 <59696174+billy1479@users.noreply.github.com>
2023-01-24 15:07:26 +0000

    eslint corrections

--- client/main2.js
+++ client/main2.js
@@ -1,16 +1,14 @@
-const { response } = require("express");
-
-rootUrl = 'http://127.0.0.1:5500/';
-async function loadCurrentFilms() {
+const rootUrl = 'http://127.0.0.1:5500/';
+async function loadCurrentFilms () {
     const currentFilmsObject = await fetch(rootUrl + 'currentFilms');
     const currentFilmsResponse = await currentFilmsObject.text();
     const x = JSON.parse(currentFilmsResponse);
-    var myKeys = Object.keys(x);
-    var currentFilmContainers = document.getElementsByClassName('currentFilmContainer');
-    var counter = 0;
-    var keyIndex = 0;
-    for (z = 0;z<currentFilmContainers.length;z++) {
-        var expression = '';
+    let myKeys = Object.keys(x);
+    const currentFilmContainers = document.getElementsByClassName('currentFilmContainer');
+    let counter = 0;
+    let keyIndex = 0;
+    for (let z = 0; z < currentFilmContainers.length; z++) {
+        let expression = '';
         if (counter == 0) {
             expression = `<img class="currentFilmImage" src="Images/${x[myKeys[keyIndex]][4]}"><div class="currentFilmDescription">
             <p>${x[myKeys[keyIndex]][0]}</p>
@@ -27,23 +25,22 @@ async function loadCurrentFilms() {
             <p>${x[myKeys[keyIndex]][3]}</p>
             </div>
             <img class="currentFilmImage" src="Images/${x[myKeys[keyIndex]][4]}">`;
-            counter = 0
+            counter = 0;
         }
         currentFilmContainers[z].innerHTML = expression;
         keyIndex += 1;
-    }
-    
+    };
 }
 
 async function loadUpcomingFilms () {
     const upcomingFilmsObject = await fetch(rootUrl + 'upcomingFilms');
     const upcomingFilmsResponse = await upcomingFilmsObject.text();
-    x = JSON.parse(upcomingFilmsResponse);
-    var myKeys = Object.keys(x);
-    var keyIndex =  0;
-    var upcomingFilmContainers = document.getElementsByClassName('UFFElement');
-    for (z = 0;z<upcomingFilmContainers.length;z++) {
-        expression = `<h3>${x[myKeys[keyIndex]][0]}</h3>
+    let x = JSON.parse(upcomingFilmsResponse);
+    let myKeys = Object.keys(x);
+    let keyIndex = 0;
+    const upcomingFilmContainers = document.getElementsByClassName('UFFElement');
+    for (let z = 0; z < upcomingFilmContainers.length; z++) {
+        let expression = `<h3>${x[myKeys[keyIndex]][0]}</h3>
         <img class="UFFImage" src="Images/${x[myKeys[keyIndex]][4]}">
         <div class="imageInformation">
         <p>${x[myKeys[keyIndex]][1]}</p>
@@ -51,19 +48,19 @@ async function loadUpcomingFilms () {
         <p>Age Rating - ${x[myKeys[keyIndex]][3]}</p>
         </div>`;
         keyIndex += 1;
-        upcomingFilmContainers[z].innerHTML = expression
+        upcomingFilmContainers[z].innerHTML = expression;
     }
 }
 
 async function loadReruns () {
     const rerunsObject = await fetch(rootUrl + 'reruns');
     const rerunsResponse = await rerunsObject.text();
-    x = JSON.parse(rerunsResponse);
-    var myKeys = Object.keys(x);
-    var keyIndex =  0;
-    var rerunsFilmContainers = document.getElementsByClassName('RRElement');
-    for (z = 0;z<rerunsFilmContainers.length;z++) {
-        expression = `<h3>${x[myKeys[keyIndex]][0]}</h3>
+    let x = JSON.parse(rerunsResponse);
+    let myKeys = Object.keys(x);
+    let keyIndex =  0;
+    const rerunsFilmContainers = document.getElementsByClassName('RRElement');
+    for (let z = 0; z < rerunsFilmContainers.length; z++) {
+        let expression = `<h3>${x[myKeys[keyIndex]][0]}</h3>
         <img class="UFFImage" src="Images/${x[myKeys[keyIndex]][5]}">
         <div class="imageInformation">
         <p>${x[myKeys[keyIndex]][1]}</p>
@@ -72,40 +69,40 @@ async function loadReruns () {
         <p>Age Rating - ${x[myKeys[keyIndex]][4]}</p>
         </div>`;
         keyIndex += 1;
-        rerunsFilmContainers[z].innerHTML = expression
+        rerunsFilmContainers[z].innerHTML = expression;
     }
 }
 
-async function loadFilmNames() {
+async function loadFilmNames () {
     const filmNamesObject = await fetch(rootUrl + 'filmNames');
     const filmNamesResponse = await filmNamesObject.text();
-    x = JSON.parse(filmNamesResponse);
-    var listElements = '';
+    let x = JSON.parse(filmNamesResponse);
+    let listElements = '';
     listElements += "<option value='False'>Select film</option>";
     x.forEach(function (y) {
         listElements += `<option value="${y[0]}">${y[0]}</option>`;
-    })  
+    });
     document.getElementById('filmSelect').innerHTML = listElements;
 }
 
-function assignForm() {
+function assignForm () {
 document.getElementById('bookingForm').addEventListener('submit', async function (e) {
     e.preventDefault();
-    var date = document.getElementById('dateInput').value;
-    var filmName = document.getElementById('filmSelect').value;
-    var filmTime = document.getElementById('screenSelect').value;
-    var nAdults = document.getElementById('adultNumberInput').value;
-    var nChildren = document.getElementById('childrenNumberInput').value;
-    var firstName = document.getElementById('firstNameInput').value;
-    var surname = document.getElementById('surnameInput').value;
-    var email = document.getElementById('emailInput').value;
-    var tempScreen;
+    let date = document.getElementById('dateInput').value;
+    let filmName = document.getElementById('filmSelect').value;
+    let filmTime = document.getElementById('screenSelect').value;
+    let nAdults = document.getElementById('adultNumberInput').value;
+    let nChildren = document.getElementById('childrenNumberInput').value;
+    let firstName = document.getElementById('firstNameInput').value;
+    let surname = document.getElementById('surnameInput').value;
+    let email = document.getElementById('emailInput').value;
+    let tempScreen;
     if (filmTime == 'screen1') {
         filmTime = '10.00AM';
         tempScreen = 'Screen 1';
     } else if (filmTime == 'screen2') {
         filmTime = '3.00PM';
-        tempScreen = 'Screen 2;'
+        tempScreen = 'Screen 2';
     } else {
         filmTime = '7.00PM';
         tempScreen = 'Screen 3';
@@ -118,11 +115,8 @@ document.getElementById('bookingForm').addEventListener('submit', async function
     } else if (document.getElementById('emailInput').value == '' || !(document.getElementById('emailInput').value.includes('@'))) {
         makeAlertBox('Please enter your email.');
     } else {
-        var x = {'email': email, 'date': date, 'film': filmName, 'time': filmTime, 'noAdults': nAdults, 'noChild': nChildren, 'firstName': firstName, 'surname': surname}
+        let x = {'email': email, 'date': date, 'film': filmName, 'time': filmTime, 'noAdults': nAdults, 'noChild': nChildren, 'firstName': firstName, 'surname': surname};
         const JSONx = JSON.stringify(x);
-        // document.getElementById('bookingForm').reset();
-        // formSlide = 0;
-        // formSlideChangeFunction();
         const formSubmission = await fetch(rootUrl + 'Bookings/MakeABooking', {
             method: 'POST',
             headers: {
@@ -135,14 +129,15 @@ document.getElementById('bookingForm').addEventListener('submit', async function
                 document.getElementById('line2').innerHTML = tempScreen
                 document.getElementById('bookingModal').style.display = 'Block';
                 document.getElementById('bookingForm').reset();
-                formSlide = 0;
+                let formSlide = 0;
                 formSlideChangeFunction();
             }
         }).catch((response) => {
             makeAlertBox('Disconnection from server - please wait for reconnection...');
-        })
+        });
     }
-})}
+});
+}
 
 function assignSearchBar() {
     document.getElementById('bookingSearch').addEventListener('submit', async function (e) {
